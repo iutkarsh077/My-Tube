a25349fc9a90d2fd1390863f55c9cdd16e500700
iutkarsh077 <utkarshsingh132002@gmail.com>
2024-01-01 10:44:24 +0530

    watchhistory controller done

--- server/src/controllers/user_controller.js
+++ server/src/controllers/user_controller.js
@@ -391,6 +391,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     throw new ApiErrors(400, "channel does not exists!");
   }
 
+  console.log(channel);
   return res
     .status(200)
     .json(
@@ -398,7 +399,61 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     );
 });
 
-console.log(channel);
+
+const getWatchHistory = asyncHandler(async(req, res) => {
+  const user = await User.aggregate([
+      {
+          $match: {
+              _id: new mongoose.Types.ObjectId(req.user._id)
+          }
+      },
+      {
+          $lookup: {
+              from: "videos",
+              localField: "watchHistory",
+              foreignField: "_id",
+              as: "watchHistory",
+              pipeline: [
+                  {
+                      $lookup: {
+                          from: "users",
+                          localField: "owner",
+                          foreignField: "_id",
+                          as: "owner",
+                          pipeline: [
+                              {
+                                  $project: {
+                                      fullName: 1,
+                                      username: 1,
+                                      avatar: 1
+                                  }
+                              }
+                          ]
+                      }
+                  },
+                  {
+                      $addFields:{
+                          owner:{
+                              $first: "$owner"
+                          }
+                      }
+                  }
+              ]
+          }
+      }
+  ])
+
+  return res
+  .status(200)
+  .json(
+      new ApiResponse(
+          200,
+          user[0].watchHistory,
+          "Watch history fetched successfully"
+      )
+  )
+})
+
 
 export {
   registerUser,
@@ -411,4 +466,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getWatchHistory
 };
